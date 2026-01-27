bf80cd621d6ed8a4cd835bad1ea38014dd9d5692
Balaji Marisetti <balajimarisetti@gmail.com>
2026-01-27 17:33:38 +0530

    Updated jsp lib

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ fn main() -> io::Result<()> {
     stdin.read_to_string(&mut buffer)?;
     println!("{}", buffer);
 
-    let mut p = buffer.chars().peekable();
-    if jsp::jsp_consume_value(&mut p) {
-        if p.peek() == None {
-            println!("Valid JSON");
-            return Ok(());
-        }
+    if let Some(x) = jsp::jsp_parse_json(&mut buffer) {
+        println!("===JSON START====");
+        println!("{}", x);
+        println!("===JSON END======");
+        Ok(())
+    } else {
+        println!("Invalid JSON");
+        Err(io::Error::new(io::ErrorKind::Other, "Invalid JSON"))
     }
-    println!("Invalid JSON");
-    Err(io::Error::new(io::ErrorKind::Other, "Invalid JSON"))
 }
